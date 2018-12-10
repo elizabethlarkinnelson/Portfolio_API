06fe7eae6f614bc8425bd0a8111da926c088c55f
Elizabeth Nelson <17551572+elizabethlarkinnelson@users.noreply.github.com>
2018-12-10 10:46:58 -0800

    Investment resource API corretly limited to get and post

--- portfolio_api_project/API/models.py
+++ portfolio_api_project/API/models.py
@@ -5,7 +5,7 @@ class Investment(models.Model):
     company = models.CharField(max_length=30)
     quantity = models.IntegerField(null=True)
     cost = models.IntegerField(null=True)
-    date = models.DateField()
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.company
@@ -15,7 +15,7 @@ class Updates(models.Model):
     investment = models.ForeignKey(Investment, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=True)
     cost = models.IntegerField(null=True)
-    date = models.DateField()
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return str(self.investment)
--- portfolio_api_project/API/resources.py
+++ portfolio_api_project/API/resources.py
@@ -1,14 +1,30 @@
 from tastypie.resources import ModelResource, ALL
 from tastypie.authorization import Authorization
-from API.models import Investment
+from API.models import Investment, Updates
 
 
 class InvestmentResource(ModelResource):
     class Meta:
         queryset = Investment.objects.all()
+        list_allowed_methods = ['get', 'post']
+        detail_allowed_methods = ['get', 'post']
         filtering = {
             'date': ALL,
         }
         resource_name = 'investment'
         authorization = Authorization()
         fields = ['company', 'quantity', 'cost']
+        excludes = ['id', 'date']
+
+
+class UpdateResource(ModelResource):
+    class Meta:
+        queryset = Updates.objects.all()
+        list_allowed_methods = ['get', 'put']
+        detail_allowed_methods = ['get', 'put']
+        filtering = {
+            'date': ALL
+        }
+        fields = ['company', 'quantity', 'cost']
+        excludes = ['id', 'date']
+        
