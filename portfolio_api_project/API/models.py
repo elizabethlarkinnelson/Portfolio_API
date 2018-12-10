7795abbe7c40da9694e8a9f4aff3cba80a16d001
Elizabeth Nelson <17551572+elizabethlarkinnelson@users.noreply.github.com>
2018-12-09 18:16:06 -0800

    Changed paid field name to cost per API specs

--- portfolio_api_project/API/models.py
+++ portfolio_api_project/API/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Investment(models.Model):
     company = models.CharField(max_length=30)
     quantity = models.IntegerField(null=True)
-    paid = models.IntegerField(null=True)
+    cost = models.IntegerField(null=True)
     date = models.DateField()
 
     def __str__(self):
@@ -14,7 +14,7 @@ class Investment(models.Model):
 class Updates(models.Model):
     investment = models.ForeignKey(Investment, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=True)
-    paid = models.IntegerField(null=True)
+    cost = models.IntegerField(null=True)
     date = models.DateField()
 
     def __str__(self):
--- portfolio_api_project/API/resources.py
+++ portfolio_api_project/API/resources.py
@@ -1,4 +1,5 @@
 from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
 from API.models import Investment
 
 
@@ -6,3 +7,5 @@ class InvestmentResource(ModelResource):
     class Meta:
         queryset = Investment.objects.all()
         resource_name = 'investment'
+        authorization = Authorization()
+        fields = ['company', 'quantity', 'paid']
